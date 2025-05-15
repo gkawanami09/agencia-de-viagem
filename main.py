2e4c54c7971afef088da2a001eaf823a5416b8a4
gkawanami09 <guilherme.kawanami@gmail.com>
2025-05-15 10:51:21 -0300

    update-desconto

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    desconto = None
+    if request.method == 'POST':
+        email = request.form.get('email')
+        if email and email.endswith('@gmail.com'):
+            desconto = 'Você ganhou 10% de desconto! Confira seu e-mail.'
+        else:
+            desconto = 'Insira um e-mail válido do Gmail para receber o desconto.'
+    return render_template('index.html', desconto=desconto)
 
 @app.route('/serra-do-cipo')
 def serra_do_cipo():
