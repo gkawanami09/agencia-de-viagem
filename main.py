d9adc2abdc4d51a008a59af15edd78c4b25bd992
gkawanami09 <guilherme.kawanami@gmail.com>
2025-05-15 10:38:18 -0300

    Update-backend
    desenvolvimento das rotas (serra-do-cipo, depoimentos, blog)

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
-from datetime import timedelta
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
+@app.route('/serra-do-cipo')
+def serra_do_cipo():
+    return render_template('/serra-do-cipo.html')
+
+@app.route('/depoimentos')
+def depoimentos():
+    return render_template('depoimentos.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
